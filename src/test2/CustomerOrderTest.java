a6f770276cda0656d6454160043516d2d633ee7b
MMolnar25 <149533663+MMolnar25@users.noreply.github.com>
2024-11-16 22:15:39 -0500

    Inventory Update Test done

--- src/test2/CustomerOrderTest.java
+++ src/test2/CustomerOrderTest.java
@@ -45,9 +45,11 @@ public class CustomerOrderTest {
 	// ToDo: Test inventory update when an order is placed
 	// Add an item to the order and reduce its stock. Verify that the inventory count is updated correctly.
 	@Test
-   	public void testLowStockAlert() {
-      		inventory.updateStock("Garlic Bread", 13);  // Only 2 left after this
-      		assertTrue(inventory.isLowStock("Garlic Bread"));
+   	public void testInventoryUpdate() {
+      		order.addItem("Pepperoni Pizza", 12.50);
+      		inventory.updateStock("Pepperoni Pizza", 1);
+      		assertEquals(9, inventory.getItemStock("Pepperoni Pizza"));
+		assertTrue(inventory.isLowStock("Garlic Bread"));
    }
 
 	// ToDo: Test low stock alert
